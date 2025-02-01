c3ca5828f0d9603e437ed648f0d252cbae590124
Andy Farmer <andy.farmer@travelopia.com>
2025-02-01 21:55:48 +0000

    Provide a Display of the Podcast with Speakers - fix lint issues

--- app/(personal)/podcasts/page.tsx
+++ app/(personal)/podcasts/page.tsx
@@ -1,26 +1,31 @@
-import { loadPaginatedPodcast } from "@/sanity/loader/loadQuery";
-import Link from "next/link";
-import { PodcastPayload, PaginatedPodcastResponse } from "@/types";
+import Link from 'next/link'
 
-export default async function PodcastListRoute({ searchParams }: { searchParams: { page?: string } }) {
-  const pageSize = parseInt(process.env.NEXT_PAGE_SIZE || '15');
-  const page = searchParams?.page ? parseInt(searchParams.page, 10) : 1;
-  const skip = (page - 1) * pageSize;
+import { loadPaginatedPodcast } from '@/sanity/loader/loadQuery'
+import { PaginatedPodcastResponse,PodcastPayload } from '@/types'
 
-  const { podcasts, podcastCount } = (await loadPaginatedPodcast(skip, pageSize)).data as PaginatedPodcastResponse;
+export default async function PodcastListRoute({
+  searchParams,
+}: {
+  searchParams: { page?: string }
+}) {
+  const pageSize = parseInt(process.env.NEXT_PAGE_SIZE || '15')
+  const page = searchParams?.page ? parseInt(searchParams.page, 10) : 1
+  const skip = (page - 1) * pageSize
+
+  const { podcasts, podcastCount } = (
+    await loadPaginatedPodcast(skip, pageSize)
+  ).data as PaginatedPodcastResponse
 
   return (
     <main>
-      <h1>Total  Podcasts {podcastCount}</h1>
-      {podcastCount === 0 && (
-        <p>No podcasts found.</p>
-      )}
+      <h1>Total Podcasts {podcastCount}</h1>
+      {podcastCount === 0 && <p>No podcasts found.</p>}
 
-     {podcasts.map((podcast) => (
-       <div key={podcast._id}>
-         <Link href={`/podcasts/${podcast.slug}`}>{podcast.title}</Link>
-       </div>
-     ))}
+      {podcasts.map((podcast) => (
+        <div key={podcast._id}>
+          <Link href={`/podcasts/${podcast.slug}`}>{podcast.title}</Link>
+        </div>
+      ))}
     </main>
   )
 }
--- components/pages/podcast/PodcastPage.tsx
+++ components/pages/podcast/PodcastPage.tsx
@@ -1,9 +1,7 @@
 import type { EncodeDataAttributeCallback } from '@sanity/react-loader'
 import Link from 'next/link'
 
-
 import { Header } from '@/components/shared/Header'
-
 import type { PodcastPayload } from '@/types'
 
 export interface PodcastPageProps {
@@ -14,7 +12,6 @@ export interface PodcastPageProps {
 export function PodcastPage({ data, encodeDataAttribute }: PodcastPageProps) {
   // Default to an empty object to allow previews on non-existent documents
   const {
-
     coverImage,
     description,
 
@@ -24,7 +21,6 @@ export function PodcastPage({ data, encodeDataAttribute }: PodcastPageProps) {
     title,
   } = data ?? {}
 
-
   return (
     <div>
       <div className="mb-20 space-y-6">
@@ -32,24 +28,21 @@ export function PodcastPage({ data, encodeDataAttribute }: PodcastPageProps) {
 
         <Header title={title} description={overview} />
 
-        
-
-          <div className="divide-inherit grid grid-cols-1 divide-y lg:grid-cols-4 lg:divide-x lg:divide-y-0">
-            <h3>Speakers</h3>
-            <ol>
-
-            {speakers && speakers.map((speaker, key) => (
-              <li key={key}>
-
-                <div className="text-md md:text-lg">
-                  <span data-sanity={encodeDataAttribute?.('speakers.name')}>
-                    {speaker.name}
-                  </span>
-                </div>
-              </li>
-            ))}
-            </ol>
-          </div>
+        <div className="divide-inherit grid grid-cols-1 divide-y lg:grid-cols-4 lg:divide-x lg:divide-y-0">
+          <h3>Speakers</h3>
+          <ol>
+            {speakers &&
+              speakers.map((speaker, key) => (
+                <li key={key}>
+                  <div className="text-md md:text-lg">
+                    <span data-sanity={encodeDataAttribute?.('speakers.name')}>
+                      {speaker.name}
+                    </span>
+                  </div>
+                </li>
+              ))}
+          </ol>
+        </div>
       </div>
     </div>
   )
