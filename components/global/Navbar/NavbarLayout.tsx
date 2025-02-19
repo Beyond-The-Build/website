2da03a14b13d6fa8872ad458f389d81ab558b652
Andy Farmer <andy.farmer@travelopia.com>
2025-02-19 14:19:38 +0000

    [feat] - Update Navigation

--- components/global/Navbar/NavbarLayout.tsx
+++ components/global/Navbar/NavbarLayout.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 
 import { resolveHref } from '@/sanity/lib/utils'
 import type { MenuItem, SettingsPayload } from '@/types'
+import Image from 'next/image'
 
 interface NavbarProps {
   data: SettingsPayload
@@ -10,7 +11,16 @@ export default function Navbar(props: NavbarProps) {
   const { data } = props
   const menuItems = data?.menuItems || ([] as MenuItem[])
   return (
-    <div className="sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur-sm md:px-16 md:py-5 lg:px-32">
+    <header className="
+    flex justify-between
+    items-center
+    sticky top-0 z-10
+    py-4
+
+    backdrop-blur-sm
+    ">
+     
+      {/* // sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur-sm md:px-16 md:py-5 lg:px-32 */}
       {menuItems &&
         menuItems.map((menuItem, key) => {
           // destructring vlaur of menuItem
@@ -33,10 +43,22 @@ export default function Navbar(props: NavbarProps) {
               }`}
               href={href}
             >
-              {menuItem.title}
+              {menuItem?._type === 'home' && (
+                <div className="flex-shrink-0 ml-6 cursor-pointer">
+
+                  <Image src="/static/logo/Beyond The Build Logo_small.webp"
+                    alt="Beyond The Build"
+                    width={50}
+                    height={50} />
+                </div>
+                
+              )}
+              {menuItem?._type !== 'home' && 
+              menuItem.title
+              }
             </Link>
           )
         })}
-    </div>
+    </header>
   )
 }
