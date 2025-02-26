8537202278b2f925be971917eb991d294506e29e
Andy Farmer <andy.farmer@travelopia.com>
2025-02-26 14:19:16 +0000

    (feat) - Update speakers list component layout

--- app/(personal)/speakers/page.tsx
+++ app/(personal)/speakers/page.tsx
@@ -9,29 +9,63 @@ import {
 
 import { loadSpeakers } from '@/sanity/loader/loadQuery'
 import { SpeakerPayload } from '@/types'
+import Image from 'next/image'
+
+import { urlForImage } from '@/sanity/lib/utils'
+import { CgProfile } from 'react-icons/cg'
 
 export default async function SpeakersListRoute() {
   const speakers = await loadSpeakers()
 
   return (
     <main>
-      <h1>Beyond the Build {speakers.data?.length} Speakers</h1>
+      <h1 className="mb-8 text-3xl">
+        Beyond the Build {speakers.data?.length} Speakers
+      </h1>
 
       {speakers.length === 0 && <p>No speakers found.</p>}
+      <div className="">
+        {speakers.data?.map((speaker, index) => (
+          <div
+            key={speaker._id}
+            className={`md:flex md:gap-4 border-indigo-200 rounded-lg mb-8 ${index % 2 === 0 ? 'md:flex-row-reverse' : 'md:flex-row'}`}
 
-      {speakers.data?.map((speaker) => (
-        <div
-          key={speaker._id}
-          className="p-4 mb-8 border-2 border-indigo-200 rounded-lg"
-        >
-          {speaker?.bio !== null ? (
-            <Link href={`/speakers/${speaker.slug}`}>{speaker.name}</Link>
-          ) : (
-            <p>{speaker.name}</p>
-          )}
-          <PortableText value={speaker.shortbio} />
-        </div>
-      ))}
+            // className="p-4 mb-8 border-2 "
+          >
+            <div className="w-16 md:w-52">
+              {speaker?.photo !== null ? (
+                <Image
+                  // className="absolute h-full w-full"
+                  className="rounded-full"
+                  alt={speaker.name}
+                  width="100"
+                  height="100"
+                  src={urlForImage(speaker.photo)
+                    ?.height(400)
+                    .width(400)
+                    .fit('crop')
+                    .url()}
+                />
+              ) : (
+                <CgProfile size="74" />
+              )}
+            </div>
+            <div className="">
+              {speaker?.bio !== null ? (
+                <Link
+                  href={`/speakers/${speaker.slug}`}
+                  className="text-2xl text-indigo-900 hover:text-indigo-700"
+                >
+                  {speaker.name}
+                </Link>
+              ) : (
+                <p>{speaker.name}</p>
+              )}
+              <PortableText value={speaker.shortbio} />
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
