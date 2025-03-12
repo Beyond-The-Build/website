c487c08c9446651fc0d01302b8f73e80acf40de5
Andy Farmer <andy.farmer@travelopia.com>
2025-03-12 12:08:27 +0000

    (feat) - Update to ESlint 9

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends : ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "prefer-template": ["error"],
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0, maxBOF: 0 }],
+    },
+
+  })
+  
+];
+
+export default eslintConfig;
