5b482c9c17a13c6bfe8d5cd349f9cd120a5404fe
Andy Farmer <andy.farmer@travelopia.com>
2025-03-22 11:02:50 +0000

    [branding] chore: ESLint fixes to Sanity loadQuery.ts

--- sanity/loader/loadQuery.ts
+++ sanity/loader/loadQuery.ts
@@ -1,34 +1,34 @@
-import 'server-only'
+import "server-only";
 
-import * as queryStore from '@sanity/react-loader'
-import { draftMode } from 'next/headers'
+import * as queryStore from "@sanity/react-loader";
+import { draftMode } from "next/headers";
 
-import { client } from '@/sanity/lib/client'
+import { client } from "@/sanity/lib/client";
 import {
   homePageQuery,
   pagesBySlugQuery,
-  projectBySlugQuery,
-  podcastBySlugQuery,
-  settingsQuery,
   paginatedPodcastQuery,
-  speakersQuery,
+  podcastBySlugQuery,
+  projectBySlugQuery,
+  settingsQuery,
   speakerBySlugQuery,
-} from '@/sanity/lib/queries'
-import { token } from '@/sanity/lib/token'
+  speakersQuery,
+} from "@/sanity/lib/queries";
+import { token } from "@/sanity/lib/token";
 import {
   HomePagePayload,
   PagePayload,
-  ProjectPayload,
   PodcastPayload,
+  ProjectPayload,
   SettingsPayload,
   SpeakerPayload,
-} from '@/types'
+} from "@/types";
 
 const serverClient = client.withConfig({
   token,
   // Enable stega if it's a Vercel preview deployment, as the Vercel Toolbar has controls that shows overlays
-  stega: process.env.VERCEL_ENV === 'preview',
-})
+  stega: process.env.VERCEL_ENV === "preview",
+});
 
 /**
  * Sets the server client for the query store, doing it here ensures that all data fetching in production
@@ -36,21 +36,21 @@ const serverClient = client.withConfig({
  * Live mode in `sanity/presentation` still works, as it uses the `useLiveMode` hook to update `useQuery` instances with
  * live draft content using `postMessage`.
  */
-queryStore.setServerClient(serverClient)
+queryStore.setServerClient(serverClient);
 
-const usingCdn = serverClient.config().useCdn
+const usingCdn = serverClient.config().useCdn;
 // Automatically handle draft mode
 export const loadQuery = (async (query, params = {}, options = {}) => {
   const {
-    perspective = (await draftMode()).isEnabled ? 'previewDrafts' : 'published',
-  } = options
+    perspective = (await draftMode()).isEnabled ? "previewDrafts" : "published",
+  } = options;
   // Don't cache by default
-  let revalidate: NextFetchRequestConfig['revalidate'] = 0
+  let revalidate: NextFetchRequestConfig["revalidate"] = 0;
   // If `next.tags` is set, and we're not using the CDN, then it's safe to cache
   if (!usingCdn && Array.isArray(options.next?.tags)) {
-    revalidate = false
+    revalidate = false;
   } else if (usingCdn) {
-    revalidate = 60
+    revalidate = 60;
   }
   return queryStore.loadQuery(query, params, {
     ...options,
@@ -58,8 +58,8 @@ export const loadQuery = (async (query, params = {}, options = {}) => {
     perspective,
     // Enable stega if in Draft Mode, to enable overlays when outside Sanity Studio
     stega: (await draftMode()).isEnabled,
-  })
-}) satisfies typeof queryStore.loadQuery
+  });
+}) satisfies typeof queryStore.loadQuery;
 
 /**
  * Loaders that are used in more than one place are declared here, otherwise they're colocated with the component
@@ -69,54 +69,54 @@ export function loadSettings() {
   return loadQuery<SettingsPayload>(
     settingsQuery,
     {},
-    { next: { tags: ['settings', 'home', 'page', 'project', 'sitelinks'] } },
-  )
+    { next: { tags: ["settings", "home", "page", "project", "sitelinks"] } }
+  );
 }
 
 export function loadHomePage() {
   return loadQuery<HomePagePayload | null>(
     homePageQuery,
     {},
-    { next: { tags: ['home', 'project', 'podcast'] } },
-  )
+    { next: { tags: ["home", "project", "podcast"] } }
+  );
 }
 
 export function loadProject(slug: string) {
   return loadQuery<ProjectPayload | null>(
     projectBySlugQuery,
     { slug },
-    { next: { tags: [`project:${slug}`] } },
-  )
+    { next: { tags: [`project:${slug}`] } }
+  );
 }
 
 export function loadPage(slug: string) {
   return loadQuery<PagePayload | null>(
     pagesBySlugQuery,
     { slug },
-    { next: { tags: [`page:${slug}`] } },
-  )
+    { next: { tags: [`page:${slug}`] } }
+  );
 }
 
 export function loadPodcast(slug: string) {
   return loadQuery<PodcastPayload | null>(
     podcastBySlugQuery,
     { slug },
-    { next: { tags: [`podcast:${slug}`] } },
-  )
+    { next: { tags: [`podcast:${slug}`] } }
+  );
 }
 
 export async function loadPaginatedPodcast(skip: number, pageSize: number) {
   return queryStore.loadQuery(
     paginatedPodcastQuery,
     { skip, pageSize },
-    { cache: 'force-cache' },
-  )
+    { cache: "force-cache" }
+  );
 }
 
 export function loadSpeakers() {
-  return loadQuery<SpeakerPayload | null>(speakersQuery, {})
+  return loadQuery<SpeakerPayload | null>(speakersQuery, {});
 }
 
 export function loadSpeakerBySlug(slug: string) {
-  return loadQuery<SpeakerPayload | null>(speakerBySlugQuery, { slug })
+  return loadQuery<SpeakerPayload | null>(speakerBySlugQuery, { slug });
 }
