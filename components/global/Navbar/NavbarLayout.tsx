66509c1fc678b6d40b1868cf1e6d8f6ffec5ec39
Andy Farmer <andy.farmer@travelopia.com>
2025-03-24 17:20:13 +0000

    [branding] feat: Branding update main navigation layout

--- components/global/Navbar/NavbarLayout.tsx
+++ components/global/Navbar/NavbarLayout.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { resolveHref } from "@/sanity/lib/utils";
 import type { MenuItem, SettingsPayload } from "@/types";
 import Image from "next/image";
+import { RiMenuFill } from "react-icons/ri";
 
 interface NavbarProps {
   data: SettingsPayload;
@@ -11,55 +12,70 @@ export default function Navbar(props: NavbarProps) {
   const { data } = props;
   const menuItems = data?.menuItems || ([] as MenuItem[]);
   return (
-    <header
-      className="
-    flex justify-between
-    items-center
-    sticky top-0 z-10
-    py-4
+    <header>
+      <nav className="flex flex-wrap items-center lg:py-0 py-2">
+        <div className="flex-1 flex justify-between items-center">
+          <a href="https://rz-codes.com" className="flex text-lg font-semibold">
+            <Image
+              src="/static/logo/Beyond The Build Logo_small.webp"
+              alt="Beyond The Build"
+              width={50}
+              height={50}
+              className="rounded-full mr-2"
+            />
+            <div className="mt-3">Beyond The Build</div>
+          </a>
+        </div>
+        <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden block">
+          <RiMenuFill className="inline" />
+        </label>
+        <input className="hidden" type="checkbox" id="menu-toggle" />
+        <div
+          className="hidden lg:flex lg:items-center lg:w-auto w-full"
+          id="menu"
+        >
+          <nav>
+            <ul className="text-xl text-center items-center gap-x-5 pt-4 md:gap-x-4 lg:text-lg lg:flex  lg:pt-0">
+              {/* <li className="py-2 lg:py-0 ">
+                  <a
+                    className=" hover:pb-4 hover:border-b-4 hover:border-yellow-400"
+                    href="#"
+                  >
+                    Blog
+                  </a>
+                </li> */}
+              {menuItems &&
+                menuItems.map((menuItem, key) => {
+                  // destructring vlaur of menuItem
+                  const { _type, title, url } = menuItem;
+                  let { slug } = menuItem;
+                  if (_type === "sitelinks") {
+                    slug = url;
+                  }
 
-    backdrop-blur-sm
-    "
-    >
-      {/* // sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur-sm md:px-16 md:py-5 lg:px-32 */}
-      {menuItems &&
-        menuItems.map((menuItem, key) => {
-          // destructring vlaur of menuItem
-          const { _type, title, url } = menuItem;
-          let { slug } = menuItem;
-          if (_type === "sitelinks") {
-            slug = url;
-          }
-
-          const href = resolveHref(_type, slug);
-          if (!href) {
-            return null;
-          }
-          return (
-            <Link
-              key={key}
-              className={`text-lg hover:text-black md:text-xl ${
-                menuItem?._type === "home"
-                  ? "font-extrabold text-eerie-black-950"
-                  : ""
-              }`}
-              href={href}
-            >
-              {menuItem?._type === "home" && (
-                <div className="flex-shrink-0 ml-6 cursor-pointer rounded-full">
-                  <Image
-                    src="/static/logo/Beyond The Build Logo_small.webp"
-                    alt="Beyond The Build"
-                    width={50}
-                    height={50}
-                    className="rounded-full"
-                  />
-                </div>
-              )}
-              {menuItem?._type !== "home" && title}
-            </Link>
-          );
-        })}
+                  const href = resolveHref(_type, slug);
+                  if (!href) {
+                    return null;
+                  }
+                  return (
+                    <li key={key} className="py-2 lg:py-0 ">
+                      <Link
+                        className={`text-lg hover:text-black md:text-xl ${
+                          menuItem?._type === "home"
+                            ? "font-extrabold text-eerie-black-950"
+                            : ""
+                        }`}
+                        href={href}
+                      >
+                        {menuItem?._type !== "home" && title}
+                      </Link>
+                    </li>
+                  );
+                })}
+            </ul>
+          </nav>
+        </div>
+      </nav>
     </header>
   );
 }
