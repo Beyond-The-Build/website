6c0a4a40b8c0d36a8cfb4c950b8c61cd1a1060a4
Andy Farmer <akf0676@sky.com>
2025-05-23 15:49:39 +0100

    [chore/dependencies] fix: update types for LiveVisualEditing

--- sanity/loader/LiveVisualEditing.tsx
+++ sanity/loader/LiveVisualEditing.tsx
@@ -1,16 +1,16 @@
-'use client'
+"use client";
 
-import { useLiveMode } from '@sanity/react-loader'
-import { VisualEditing } from 'next-sanity'
-import { useEffect } from 'react'
+import { useLiveMode } from "@sanity/react-loader";
+import { VisualEditing } from "next-sanity";
+import type { SanityClient } from "sanity";
 
-import { client } from '@/sanity/lib/client'
+import { client } from "@/sanity/lib/client";
 
 // Always enable stega in Live Mode
-const stegaClient = client.withConfig({ stega: true })
+const stegaClient = client.withConfig({ stega: true }) as SanityClient;
 
 export default function LiveVisualEditing() {
-  useLiveMode({ client: stegaClient })
+  useLiveMode({ client: stegaClient });
 
-  return <VisualEditing />
+  return <VisualEditing />;
 }
